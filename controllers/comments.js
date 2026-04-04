fdd86d8324a25cc0364b7a81c27de6b8f5af2d63
Luciano Vaccaro <luciano.vaccaro510@gmail.com>
2026-04-04 20:18:40 +0200

    feat: enhance comment handling with improved error responses and validation

--- controllers/comments.js
+++ controllers/comments.js
@@ -12,7 +12,7 @@ export async function findAll(req, res) {
     }
     const post = await BlogPost.findById(blogPostId);
     if (!post) {
-      return res.status(404).json({ message: "blog Not Found" });
+      return res.status(404).json({ message: "blogPost Not Found" });
     }
     res.status(200).json(post.comments);
   } catch (error) {
@@ -22,6 +22,27 @@ export async function findAll(req, res) {
 
 export async function findById(req, res) {
   try {
+    const { blogPostId, id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(blogPostId)) {
+      return res.status(400).json({
+        message: "Invalid blogPost Id",
+      });
+    }
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        message: "Invalid comment Id",
+      });
+    }
+    const blogPost = await BlogPost.findById(blogPostId);
+    if (!blogPost) {
+      return res.status(404).json({ message: "blogPost Not Found" });
+    }
+
+    const comment = blogPost.comments.id(id);
+    if (!comment) {
+      return res.status(404).json({ message: "comment Not Found" });
+    }
+    res.status(200).json(comment);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -48,12 +69,63 @@ export async function create(req, res) {
 }
 export async function elimina(req, res) {
   try {
+    const { blogPostId, id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(blogPostId)) {
+      return res.status(400).json({
+        message: "Invalid blogPost Id",
+      });
+    }
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        message: "Invalid comment Id",
+      });
+    }
+
+    const blogPost = await BlogPost.findById(blogPostId);
+    if (!blogPost) {
+      return res.status(404).json({ message: "blogPost Not Found" });
+    }
+    const comment = blogPost.comments.id(id);
+    if (!comment) {
+      return res.status(404).json({ message: "comment Not Found" });
+    }
+    comment.deleteOne();
+    await blogPost.save();
+    res.status(200).json({ message: "Comment Deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 }
 export async function update(req, res) {
   try {
+    const { blogPostId, id } = req.params;
+    const { text, author } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(blogPostId)) {
+      return res.status(400).json({
+        message: "Invalid blogPost Id",
+      });
+    }
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        message: "Invalid comment Id",
+      });
+    }
+
+    const blogPost = await BlogPost.findById(blogPostId);
+    if (!blogPost) {
+      return res.status(404).json({ message: " Blog Post Not Found" });
+    }
+
+    const comment = blogPost.comments.id(id);
+    if (!comment) {
+      return res.status(404).json({ message: "comment Not Found" });
+    }
+    comment.text = text;
+    comment.author = author;
+    await blogPost.save();
+    res.status(200).json(comment);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
